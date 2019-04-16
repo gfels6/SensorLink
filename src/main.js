a9dfe4b6eab2110f8956c11a029d5cb7349cc3a2
gfels6 <sascha.gfeller@gmail.com>
2019-04-16 10:54:35 +0200

    added libraries (vuex, router, axios, highchart)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import './plugins/vuetify'
 import App from './App.vue'
+//import router from './router'
+import HighchartsVue from 'highcharts-vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Vuex)
+Vue.use(HighchartsVue)
