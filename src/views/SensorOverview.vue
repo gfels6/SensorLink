8ae0c39ad3ca325d13498d47df1875992bbc0511
gfels6 <sascha.gfeller@gmail.com>
2019-06-11 17:40:17 +0200

    added linking and unlinking patients and sensors

--- src/views/SensorOverview.vue
+++ src/views/SensorOverview.vue
@@ -25,12 +25,50 @@
                     <div v-if="sensor.sensorType=='everion'"><b>Mögliche Messwerte: </b>Puls, Sauerstoffsättigung, Atemfrequenz, Herzvariabilität</div>
                     <div v-if="sensor.linkedPatId!=null"><b>Verbindung: </b>Patient {{sensor.linkedPatId}} mit diesem Sensor verbunden</div>
                     <div v-if="sensor.linkedPatId==null"><b>Verbindung: </b>Kein Patient mit diesem Sensor verbunden</div>
+                    <v-form
+                      v-if="sensor.linkedPatId==null"
+                      ref="`form + i`"
+                      v-model="valid"
+                      lazy-validation
+                    >
+                      <v-autocomplete
+                          v-model="mdlPatient"
+                          :items="patientList"
+                          :label="`Wähle einen Patienten zum verbinden aus`"
+                          :rules="[v => !!v || 'Patient ist erforderlich!']"
+                          return-object
+                          required
+                      >
+                        <template slot="selection" slot-scope="data">
+                            <v-list-tile-content>
+                                <v-list-tile-title v-html="data.item.firstname +' '+ data.item.lastname"></v-list-tile-title>
+                            </v-list-tile-content>
+                        </template>
+                        <template slot="item" slot-scope="data">
+                            <v-list-tile-content>
+                                <v-list-tile-title v-html="data.item.firstname +' '+ data.item.lastname"></v-list-tile-title>
+                            </v-list-tile-content>
+                        </template>
+                      </v-autocomplete>
+                      <v-btn
+                        :disabled="!valid"
+                        color="success"
+                        @click="validate(sensor, i)"
+                      >
+                        Verbinden
+                      </v-btn>
+                    </v-form>
+                    <v-btn
+                      v-if="sensor.linkedPatId!=null"
+                      color="success"
+                      @click="deleteSensor(sensor, sensor.linkedPatId)"
+                    >
+                      Sensor entfernen
+                    </v-btn>
                   </v-card-text>
                 </v-card>
               </v-expansion-panel-content>
             </v-expansion-panel>
-        
-
     </v-container>
       
   </div>
@@ -39,11 +77,16 @@
 <script>
 
 import {mapState} from 'vuex'
+import axios from 'axios'
+
+const SL_BASE_URL = 'http://patientpath.i4mi.bfh.ch:3000/api/';
 
 export default {
   data(){
     return {
-
+      mdlPatient: '',
+      patList: [],
+      valid: true,
     }
   },
   components: {
@@ -51,20 +94,82 @@ export default {
   },
 
   computed : {
+    ...mapState(['patientList']),
     ...mapState(['sensorList']),
   },
 
   methods: {
     addSensor() {
       this.$router.push('/addSensor')
+    },
+
+    validate(sensor,i) {
+      //console.log(sensor, i, this.mdlPatient);
+      if(this.mdlPatient != '') {
+        this.connectSensor(sensor, this.mdlPatient);
+      }
+    },
+
+    connectSensor(sensor, patient){
+      axios({url: SL_BASE_URL + 'patients/' + patient.patId + '/linkSensor', 
+      method: 'POST',
+      data: {
+        "sensorIdentifier": sensor.sensorIdentifier
+      },
+      headers: { "Content-Type": "application/json", "Authorization": this.$store.state.token},
+      })
+      .then((response) => {
+
+        this.$router.go(0);
+        /*
+        if(response.status == "200") {
+          this.snackbar = true;
+          setTimeout(() =>{
+            this.$router.push('/sensorOverview')
+          }, 3000);
+        } */
+      })
+      .catch(err => {
+        console.log("Error: " + err.statusCode + ": " + err.statusMessage)
+      }) 
+    },
+
+    deleteSensor(sensor, patId) {
+      axios({url: SL_BASE_URL + 'patients/' + patId + '/unlinkSensor', 
+      method: 'POST',
+      data: {
+        "sensorIdentifier": sensor.sensorIdentifier
+      },
+      headers: { "Content-Type": "application/json", "Authorization": this.$store.state.token},
+      })
+      .then((response) => {
+
+        this.$router.go(0);
+        /*
+        if(response.status == "200") {
+          this.snackbar = true;
+          setTimeout(() =>{
+            this.$router.push('/sensorOverview')
+          }, 3000);
+        } */
+      })
+      .catch(err => {
+        console.log("Error: " + err.statusCode + ": " + err.statusMessage)
+      }) 
     }
+
   },
 
   mounted() {
+    this.$store.dispatch('getAllPatients')
+    .catch((error) => {
+      console.log("Error: " + error.statusCode + ": " + error.statusMessage)
+    })
     this.$store.dispatch('getAllSensors')
     .catch((error) => {
       console.log("Error: " + error.statusCode + ": " + error.statusMessage)
     })
+
   }
     
   }
